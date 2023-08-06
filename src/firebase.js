bde76af44ef222b699b3a73be6d25405c4459f1f
RamFarid <workprojects22@gmail.com>
2023-08-06 18:15:49 +0300

    Put firebase config in env variable

--- src/firebase.js
+++ src/firebase.js
@@ -6,15 +6,7 @@ import { getFirestore } from 'firebase/firestore'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyBDcRrFolUa1FzmNKGm46wsOk9VPHcIysI',
-  authDomain: 'todo-2a588.firebaseapp.com',
-  projectId: 'todo-2a588',
-  storageBucket: 'todo-2a588.appspot.com',
-  messagingSenderId: '159716669424',
-  appId: '1:159716669424:web:a1c4c14840b0a26c390f35',
-}
-
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIRESBASE_CONFIG)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
